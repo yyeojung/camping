7c72557c91db678bf232dcfdecdb4bc7539bcee6
yyeojung <hongshii0i0i@gmail.com>
2024-07-09 21:30:14 +0900

    드롭다운 커스텀

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -8,11 +8,28 @@ import { AREA0, regionMapping } from "../../commons/data/subArea";
 //   { value: "vanilla", label: "Vanilla" },
 // ];
 
+// const customSelect = { 스타일 주기
+//   control: (provided: any) => ({
+//     ...provided,
+//     background: "Red",
+//   }),
+//   option: (provided: any, state: { isSelected: boolean }) => ({
+//     ...provided,
+//     backgroundColor: state.isSelected ? "#000" : "green",
+//   }),
+//   indecatorseparateor: (provided: any) => ({
+//     ...provided,
+//     display: "none",
+//   }),
+// };
+
+// 옵션 타입 정의
 interface Option {
   value: string;
   label: string;
 }
 
+// 드롭다운 옵션
 const createOptions = (areas: string[]): Option[] =>
   areas.map((area) => ({
     value: area,
@@ -20,32 +37,47 @@ const createOptions = (areas: string[]): Option[] =>
   }));
 
 export default function DropDown() {
+  // 광역시
   const [selectedRegion, setSelectedRegion] =
     useState<SingleValue<Option>>(null);
+  // 행정구역
   const [subRegions, setSubRegions] = useState<Option[]>([]);
+  // 광역시 선택시 행정구역 리셋
+  const [subRegionReset, setSubRegionReset] =
+    useState<SingleValue<Option>>(null);
 
-  const onChangeRegion = (seletedOption: SingleValue<Option>) => {
-    setSelectedRegion(seletedOption);
-    if (seletedOption) {
+  // 광역시도 onChange
+  const onChangeRegion = (selectedOption: SingleValue<Option>) => {
+    setSelectedRegion(selectedOption);
+    setSubRegionReset(null);
+    if (selectedOption) {
       const subAreas =
-        regionMapping[seletedOption.value as keyof typeof regionMapping];
+        regionMapping[selectedOption.value as keyof typeof regionMapping];
       setSubRegions(createOptions(subAreas));
     } else {
       setSubRegions([]);
     }
   };
 
-  // !!!! 시 선택하면 하위 지역 리셋하기
+  // 행정구역 onChange
+  const onChangeSubRegion = (selectedOption: SingleValue<Option>) => {
+    setSubRegionReset(selectedOption);
+  };
 
   return (
     <>
       <Select
+        instanceId="main_select"
         options={createOptions(AREA0)}
         onChange={onChangeRegion}
         placeholder="광역시도 선택"
+        // styles={customSelect}
       />
       <Select
+        instanceId="sub_select"
         options={subRegions}
+        value={subRegionReset}
+        onChange={onChangeSubRegion}
         placeholder="하위 지역 선택"
         isDisabled={!selectedRegion}
       />
